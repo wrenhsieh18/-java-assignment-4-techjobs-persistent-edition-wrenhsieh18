cde75a8b9a099efe65c9863521a67b10680da327
Jenhsiang Hsieh (Wren) <wrenhsieh18@gmail.com>
2022-03-10 15:19:13 -0600

    Part1

--- queries.sql
+++ queries.sql
@@ -1,4 +1,8 @@
--- Part 1: Test it with SQL
+-- Part 1: Test it with SQL (SQL TASK: At this point, you will have one table, job. In queries.sql under “Part 1”, list the columns and their data types in the table.)
+   id int PRIMARY_KEY
+   employer varchar(255)
+   name varchar(255)
+   skills varchar(255)
 
 -- Part 2: Test it with SQL
 
